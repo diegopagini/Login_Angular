30b64e8dc9627e765bb24c1b84042ffd5ec2a659
Diego Pagini <diegopagini777@gmail.com>
2021-01-02 14:42:30 -0300

    terminado

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -60,6 +60,9 @@ export class AuthService {
   private guardarToken(idToken: string) {
     this.userToken = idToken;
     localStorage.setItem('token', idToken);
+    let hoy = new Date();
+    hoy.setSeconds(3600);
+    localStorage.setItem('expira', hoy.getTime().toString());
   }
 
   leerToken() {
@@ -73,6 +76,17 @@ export class AuthService {
   }
 
   estaAutenticado(): boolean {
-    return this.userToken.length > 2;
+    if (this.userToken.length < 2) {
+      return false;
+    }
+    const expira = Number(localStorage.getItem('expira'));
+    const expiraDate = new Date();
+    expiraDate.setTime(expira);
+
+    if (expiraDate > new Date()) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
